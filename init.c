d0efc8f389aceb46741570e3cd0e0c2419cba573
Nguyễn Viết Minh Đức <mineshift98@gmail.com>
2023-03-11 16:16:13 +0700

    new

---
+++ init.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <accctrl.h>
+
+int main()
+{
+	int num;
+	FILE *fptr;
+
+	fptr=fopen("1.txt","a+");
+	if(fptr == NULL) 
+	{
+		printf("Error! Opening file");
+		exit(1);
+	}
+	else {
+	printf("Enter num: ");
+   	scanf("%d",&num);
+   	fprintf(fptr," %d",num);
+  	fclose(fptr);
+	}
+   return 0;
+}
